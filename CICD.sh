03892060a9bdbfeb3c0756d26269c2894776a699
Alex Davis <121192951+AlexTechLegend@users.noreply.github.com>
2023-11-09 15:16:04 -0800

    Create CICD.sh

---
+++ CICD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo cp /usr/share/nginx/html/current/index.html /usr/share/nginx/html/archive/index.html
+
+echo "Archiving last version..."
+sleep 1s
+
+sudo cp /usr/share/nginx/html/edits/index.html /usr/share/nginx/html/current/index.html
+echo "Uploading latest version..."
+sleep 1s
+
+#############################
+
+sudo docker build -t nginx .
+
+sudo docker tag nginx pnnlacr.azurecr.io/nginx:latest
+
+sudo docker push pnnlacr.azurecr.io/nginx
